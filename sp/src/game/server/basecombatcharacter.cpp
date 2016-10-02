d5fe2fd01cd69bbee5762bf9f6be37acb298df95
Anton Nesterov <ntrf@znscode.net>
2016-10-02 18:17:46 +0300

    Damage reduction is set to 6 instead of 15 by default

--- sp/src/game/server/basecombatcharacter.cpp
+++ sp/src/game/server/basecombatcharacter.cpp
@@ -2459,7 +2459,7 @@ int CBaseCombatCharacter::OnTakeDamage( const CTakeDamageInfo &info )
 	}
 }
 
-static ConVar bla_damagefraction("bla_damagefraction", "15", 
+static ConVar bla_damagefraction("bla_damagefraction", "6", 
 								 FCVAR_DEMO | FCVAR_REPLICATED | FCVAR_ARCHIVE,
 								 "Fraction by which damage dealt to the "
 								 "player should be reduced. The value must "
