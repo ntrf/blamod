5623ba0e4f935b4ee91fe5d0314b39e8b742a6bb
Anton Nesterov <ntrf@znscode.net>
2016-11-27 17:04:30 +0300

    Unlocked some development-only `sv_*` variables
    Conflicts:
    	sp/src/game/client/c_baseentity.cpp
    	sp/src/game/shared/baseplayer_shared.cpp
    	sp/src/game/shared/cam_thirdperson.cpp
    	sp/src/game/shared/movevars_shared.cpp

--- sp/src/game/client/c_baseentity.cpp
+++ sp/src/game/client/c_baseentity.cpp
@@ -3156,7 +3156,7 @@ void C_BaseEntity::Simulate()
 }
 
 // Defined in engine
-static ConVar cl_interpolate( "cl_interpolate", "1.0f", FCVAR_USERINFO | FCVAR_DEVELOPMENTONLY );
+static ConVar cl_interpolate( "cl_interpolate", "1.0f", FCVAR_USERINFO );
 
 // (static function)
 void C_BaseEntity::InterpolateServerEntities()
--- sp/src/game/shared/cam_thirdperson.cpp
+++ sp/src/game/shared/cam_thirdperson.cpp
@@ -47,7 +47,7 @@ void ThirdPersonChange( IConVar *pConVar, const char *pOldValue, float flOldValu
 	ToggleThirdPerson( var.GetBool() );
 }
 
-ConVar cl_thirdperson( "cl_thirdperson", "0", FCVAR_NOT_CONNECTED | FCVAR_USERINFO | FCVAR_ARCHIVE | FCVAR_DEVELOPMENTONLY, "Enables/Disables third person", ThirdPersonChange  );
+ConVar cl_thirdperson( "cl_thirdperson", "0", FCVAR_NOT_CONNECTED | FCVAR_USERINFO | FCVAR_ARCHIVE, "Enables/Disables third person", ThirdPersonChange  );
 
 #endif
 
