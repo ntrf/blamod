62b3b77855f2f9763cb99079f9bde6eef27bc427
Anton Nesterov <ntrf@znscode.net>
2017-06-12 11:30:07 +0300

    Game movement feature: get stuck in displacements and OOB with +walk

--- sp/src/game/shared/gamemovement.cpp
+++ sp/src/game/shared/gamemovement.cpp
@@ -2658,7 +2658,7 @@ int CGameMovement::TryPlayerMove( Vector *pFirstDest, trace_t *pFirstTrace )
 				// If we detect getting stuck, don't allow the movement
 				trace_t stuck;
 				TracePlayerBBox( pm.endpos, pm.endpos, PlayerSolidMask(), COLLISION_GROUP_PLAYER_MOVEMENT, stuck );
-				if ((stuck.startsolid || stuck.fraction != 1.0f) && stuck.contents != 0)
+				if ((stuck.startsolid || stuck.fraction != 1.0f) && (stuck.contents != 0 || (mv->m_nButtons & IN_WALK)))
 				{
 					//Msg( "Player will become stuck!!!\n" );
 					VectorCopy (vec3_origin, mv->m_vecVelocity);
